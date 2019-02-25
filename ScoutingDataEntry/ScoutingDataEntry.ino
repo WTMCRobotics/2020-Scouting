fe07fe151c939ecfc164ab7da2796d1f6c74cef6
Tom Harkaway <tlsystems@comcast.net>
2019-02-25 12:16:58 -0500

    Add .gitignore file Re-work serial commands to two character each, case insensitive Add code to count and report button presses Add code to debounce buttons

--- ScoutingDataEntry/ScoutingDataEntry.ino
+++ ScoutingDataEntry/ScoutingDataEntry.ino
@@ -1,19 +1,59 @@
-// Visual Micro is in vMicro>General>Tutorial Mode
-// 
 /*
-    Name:       ScoutingDataEntry.ino
-    Created:	1/17/2019 1:08:07 PM
-    Author:     tls-mobile7\tomh
+  Data Entry program for FIRST Robotics Competitions
+	Serial Commands
+	  Match Mode
+		"MR"  reset - clear all counts
+		"MW"  countdown - flash LED in prep for game starting
+		"MA"  auton - enter auton mode
+		"MT"  teleop - enter teleop mode 
+		"MD"  done - finish match
+	  Read data
+		"RV"  read version string
+		"RM"  read mode
+		"RS"  read ID switches
+		"RB"  read button data
+		"RA"  read auton counts
+		"RT"  read teleop counts
+	  LED Control
+		"L0"  LED Off
+		"L1"  LED On
+		"LT"  LED Toggle
+		"LF"  LED Flash
 */
 
+#define NUMBER_OF_BUTTONS 8
+#define DEBOUNCE_LENGTH  14
+
+char version[]  = "v0.3";
+
+// LED Commands
 enum
 {
-	eLED_ON,
-	eLED_OFF,
-	eLED_Toggle
+	ledOff,
+	ledOn,
+	ledToggle,
+	ledFlash
+};
+
+// Match Modex
+enum 
+{
+	matchReset	= 0,
+	matchWait	= 1,
+	matchAuton	= 2,
+	matchTeleop	= 3,
+	matchDone	= 4
 };
 
 bool _bFlashLED = false;
+byte _matchMode = matchDone;
+
+byte _buttons;
+byte _switchData;
+char _responseBuffer[64];
+byte _autonCount[NUMBER_OF_BUTTONS];
+byte _teleopCount[NUMBER_OF_BUTTONS];
+
 
 void setup()
 {
@@ -33,97 +73,194 @@ void setup()
 	pinMode(12, INPUT_PULLUP);
 	pinMode(13, OUTPUT);
 	pinMode(19, OUTPUT);
-	digitalWrite(19,HIGH);
+	digitalWrite(19, HIGH);
+
+	PollSwitches();
 
 	Serial.begin(115200);
 }
 
+byte _presses;
+byte _lastPresses;
 
 void loop()
 {
+	static char ch;
 	static uint32_t lastMillis = 0;
-	static uint8_t buttons;
-	static uint8_t switches;
-	static char responseBuffer[3];
+//	Serial.println(micros());
 
-	buttons  = PollButtons();
-	switches = PollSwitches();
+	DebounceButtons();
+	PollButtons();
 
-	if (_bFlashLED && (millis() - lastMillis > 500))
+	if ((millis() - lastMillis) > 500)
 	{
 		lastMillis = millis();
-		SetLED(eLED_Toggle);
+		if (_bFlashLED)	
+			SetLED(ledToggle);
 	}
 	
-	if (Serial.available() > 0)
+	ProcessSerialCommands();
+}
+
+void ProcessSerialCommands()
+{
+	if (Serial.peek() == -1)
+		return;
+
+	char ch = MakeUpper(Serial.read());
+	switch (ch)
 	{
-		switch(Serial.read())
+		// read switches and buttons
+	case 'M':
+		switch (GetChar())
 		{
-		case 's':
-			FormatResponse(switches, responseBuffer);
-			Serial.println(responseBuffer);
-			SetLED(eLED_Toggle);
+		case 'R':	// reset
+			ResetAllCounts();
+			_matchMode = matchReset;
 			break;
-
-		case 'b':
-			FormatResponse(buttons, responseBuffer);
-			Serial.println(responseBuffer);
+		case 'W':	// countdown
+			SetLED(ledFlash);
+			_matchMode = matchWait;
 			break;
-
-		case '0':
-			_bFlashLED = false;
-			SetLED(eLED_OFF);
+		case 'A':	// auton
+			SetLED(ledOn);
+			_matchMode = matchAuton;
 			break;
-
-		case '1':
-			_bFlashLED = false;
-			SetLED(eLED_ON);
+		case 'T':	// teleop
+			SetLED(ledOff);
+			_matchMode = matchTeleop;
 			break;
-
-		case 'f':
-			_bFlashLED = true;
-			break;
-
-		case 'v':
-			Serial.println("v0.02");
-			break;
-
-		default:
+		case 'E':	// end of match
+			SetLED(ledOff);
+			_matchMode = matchDone;
 			break;
 		}
+		break;
+
+		// read 
+	case 'R':
+		char* buffer;
+		switch (GetChar())
+		{
+		case 'V': buffer = version;							break;
+		case 'M': buffer = GetModeResponse();				break;
+		case 'S': buffer = GetByteResponse(_switchData);	break;
+		case 'B': buffer = GetByteResponse(_buttons);	break;
+		case 'A': buffer = GetCountResponse(_autonCount);	break;
+		case 'T': buffer = GetCountResponse(_teleopCount);	break;
+		}
+		Serial.println(buffer);
+		break;
+
+		// control led
+	case 'L':
+		switch (GetChar())
+		{
+		case '0': SetLED(ledOff);		break;
+		case '1': SetLED(ledOn);		break;
+		case 'T': SetLED(ledToggle);	break;
+		case 'F': SetLED(ledFlash);		break;
+		}
+		break;
 	}
+}
+
+char MakeUpper(char ch)
+{
+	if (ch >= 'a' && ch <= 'z')
+		return ch -= 0x20;
+	else
+		return ch;
+}
+
+char GetChar()
+{
+	while (Serial.peek() == -1);
+	return MakeUpper(Serial.read());
 }
 
 void SetLED(int action)
 {
 	switch (action)
-	{
-		case eLED_ON:
+	{	
+		case ledOn:
+			_bFlashLED = false;
 			digitalWrite(19, LOW);
 			break;
-		case eLED_OFF:
+		case ledOff:
+			_bFlashLED = false;
 			digitalWrite(19, HIGH);
 			break;
-		case eLED_Toggle:
+		case ledToggle:
 			digitalWrite(19, !digitalRead(19));
+			break;
+		case ledFlash:
+			_bFlashLED = true;
 			break;
 		default:
 			break;
 	}
 }
 
-uint16_t PollButtons()
+void PollButtons()
 {
-	uint16_t data = 0;
-	if (!digitalRead( 8)) data |= 0x01;
-	if (!digitalRead( 9)) data |= 0x02;
-	if (!digitalRead( 7)) data |= 0x04;
-	if (!digitalRead( 6)) data |= 0x08;
-	if (!digitalRead( 5)) data |= 0x10;
-	if (!digitalRead( 4)) data |= 0x20;
-	if (!digitalRead( 3)) data |= 0x40;
-	if (!digitalRead( 2)) data |= 0x80;
-	return data;
+	static uint16_t lastData = 0;
+
+	uint16_t presses  = (_buttons ^ lastData) & _buttons;	
+	uint16_t releases = (_buttons ^ lastData) & ~_buttons;
+	lastData = _buttons;
+
+	_presses = presses;
+
+	for (int i = 0; i < NUMBER_OF_BUTTONS; i++)
+	{
+		if (presses & 0x01 == 1)
+		{
+			if (_matchMode == matchAuton)
+				_autonCount[i]++;
+			else if (_matchMode == matchTeleop)
+				_teleopCount[i]++;
+		}
+		presses = presses >> 1;
+	}
+}
+
+void DebounceButtons()
+{
+	static uint16_t buttonHistory[8];
+	static uint16_t debounceMask = (1 << DEBOUNCE_LENGTH) - 1;
+
+	byte buttonBit = 0x01;
+	byte buttonVal = 0;
+
+	// get state of buttons
+	if (!digitalRead(8)) buttonVal |= 0x01;
+	if (!digitalRead(9)) buttonVal |= 0x02;
+	if (!digitalRead(7)) buttonVal |= 0x04;
+	if (!digitalRead(6)) buttonVal |= 0x08;
+	if (!digitalRead(5)) buttonVal |= 0x10;
+	if (!digitalRead(4)) buttonVal |= 0x20;
+	if (!digitalRead(3)) buttonVal |= 0x40;
+	if (!digitalRead(2)) buttonVal |= 0x80;
+
+	for (int i = 0; i < NUMBER_OF_BUTTONS; i++)
+	{
+		buttonHistory[i] <<= 1;
+		if ((buttonVal & 0x01) == 1)
+		{
+			buttonHistory[i] |= 0x0001;
+			if ((buttonHistory[i] & debounceMask) == debounceMask)
+			_buttons |= buttonBit;
+		}
+		else
+		{
+			buttonHistory[i] &= 0xfffe;
+			if ((buttonHistory[i] & debounceMask) == 0x0000)
+				_buttons &= ~buttonBit;
+		}
+		buttonVal >>= 1;	
+		buttonBit <<= 1;
+	}
 }
 
 uint16_t PollSwitches()
@@ -135,11 +272,49 @@ uint16_t PollSwitches()
 	return data;
 }
 
-void FormatResponse(uint16_t inputData, char* pBuffer)
+void ResetAllCounts()
 {
+	for (int i = 0; i < NUMBER_OF_BUTTONS; i++)
+	{
+		_autonCount[i] = 0;
+		_teleopCount[i] = 0;
+	}
+}
+
+char* GetModeResponse()
+{
+	switch (_matchMode)
+	{
+		case matchReset:	return "Reset";		break;
+		case matchWait:		return "Waiting";	break;
+		case matchAuton:	return "Auton";		break;
+		case matchTeleop:	return "Teleop";	break;
+		case matchDone:		return "Done";		break;
+	}
+	return "\0";
+}
+
+char* GetByteResponse(uint16_t inputData)
+{
+	char* pBuffer = _responseBuffer;
 	*pBuffer++ = Bin2Hex((inputData >> 4) & 0x0f);
 	*pBuffer++ = Bin2Hex((inputData >> 0) & 0x0f);
 	*pBuffer = '\0';
+	return _responseBuffer;
+}
+
+char* GetCountResponse(byte* countBuf)
+{
+	char* pBuffer = _responseBuffer;
+	for (int i = 0; i < NUMBER_OF_BUTTONS; i++)
+	{
+		byte count = *countBuf++;
+		*pBuffer++ = Bin2Hex((count >> 4) & 0x0f);
+		*pBuffer++ = Bin2Hex((count >> 0) & 0x0f);
+		*pBuffer++ = ',';
+	}
+	*pBuffer = '\0';
+	return _responseBuffer;
 }
 
 char Bin2Hex(byte inData)
